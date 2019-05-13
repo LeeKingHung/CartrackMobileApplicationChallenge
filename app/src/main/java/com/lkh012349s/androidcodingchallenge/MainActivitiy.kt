d678d79dec9b1cd28fe47fa0d94218b3d9fb8f39
LEEKINGHUNG\Lee King Hung <slkhung@hotmail.com>
2019-05-13 23:26:15 +0800

    - Initial commit.

---
+++ app/src/main/java/com/lkh012349s/androidcodingchallenge/MainActivitiy.kt
@@ -0,0 +1,11 @@
+package com.lkh012349s.androidcodingchallenge
+
+import android.support.v7.app.AppCompatActivity
+import org.androidannotations.annotations.EActivity
+
+@EActivity(R.layout.activity_main)
+class MainActivitiy:AppCompatActivity() {
+
+
+
+}
